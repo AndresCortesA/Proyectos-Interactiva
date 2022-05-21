9e1db6d9a679fec026e1f6154ece80800d2aeacf
AndresCorte2399 <101019474+AndresCorte2399@users.noreply.github.com>
2022-05-21 17:25:39 -0500

    Add files via upload

---
+++ ConversorMedida.java
@@ -0,0 +1,63 @@
+package ConversorDeMedida;
+
+import java.util.StringTokenizer;
+
+import javax.swing.JOptionPane;
+
+public class ConversorMedida {
+//	 Desarrolle una aplicación en Java que permita convertir entre kilos, libras y gramos.
+//	 También de centímetros a metros y de metros a centímetros. Debe funcionar a través de
+//	 JOptionPane y funcionar con entradas en este formato (sin comillas) “convertir 3 kilos a libras”,
+//	 “convertir 3 kilos a gramos”, “convertir 105 centímetros a metros”, etc. La respuesta debe ser
+//	 presentada también a través de JOptionPane. En caso de entradas en formatos diferentes, la
+//	 aplicación debe informar dicho error.
+	public static void main(String[] args) {
+		while(true) {
+			try {
+				String value = JOptionPane.showInputDialog("Introduce el valor a trabajar: ");
+				StringTokenizer st = new StringTokenizer(value,",");
+				if(st.countTokens()==3) {
+					double medirTr= Double.parseDouble(st.nextToken());
+					String valor1 = st.nextToken();
+					String valor2 = st.nextToken();
+					//convertir entre kilos, libras y gramos.
+					if( (valor1.equals("kilos")) && (valor2.equals("libras")) ) {
+						double pasarLibras = ( (medirTr * 1000)/500 );
+						JOptionPane.showMessageDialog(null, "Son: " + pasarLibras+" libras");
+					}else if( (valor1.equals("kilos")) && (valor2.equals("gramos")) ) {
+						double pasarKGramos= (medirTr*1000);
+						JOptionPane.showMessageDialog(null, "Son:"+pasarKGramos+" gramos");
+					}else if( (valor1.equals("libras")) && (valor2.equals("kilos")) ){
+						double pasarKilos =(( (medirTr*1000)/2)/1000 );
+						JOptionPane.showMessageDialog(null, "Son: "+ pasarKilos + " kilo");
+					}else if( (valor1.equals("libras")) && (valor2.equals("gramos")) ) {
+						double pasarLGramos = (medirTr*500);
+						JOptionPane.showMessageDialog(null, "Son: " + pasarLGramos+" gramos");
+					}else if( (valor1.equals("gramos")) && (valor2.equals("kilos")) ) {
+						double pasarGKilos =(medirTr/1000);
+						JOptionPane.showMessageDialog(null, "Son: "+pasarGKilos+" kilos");
+					}else if( (valor1.equals("gramos")) && (valor2.equals("libras")) ) {
+						double pasarGLibras = medirTr/500;
+						JOptionPane.showMessageDialog(null, "Son: "+pasarGLibras+" libras");
+						//convertir centímetros a metros y de metros a centímetros.
+					}else if( (valor1.equals("cm")) && (valor2.equals("m")) ) {
+						double centimetroM = medirTr/100;
+						JOptionPane.showMessageDialog(null, "Son: "+centimetroM+" metros");
+					}else if( (valor1.equals("m")) && (valor2.equals("cm")) ) {
+						double metroCM = medirTr * 100;
+						JOptionPane.showMessageDialog(null, "Son: "+metroCM+" centimetros");
+					}else JOptionPane.showMessageDialog(null, "valor no permitido");
+					
+				}else if(st.countTokens()==1) {
+					if(st.nextToken().equals("salir")) {
+						break;
+					}else JOptionPane.showMessageDialog(null, "Valor no permitido");
+				}
+			} catch (NullPointerException e) {
+				// TODO: handle exception
+				JOptionPane.showMessageDialog(null, "Termina la ejecucion");
+				break;
+			}
+		}
+	}
+}
